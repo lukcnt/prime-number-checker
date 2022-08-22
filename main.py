42a004406be32785121de2049a90f14aaa7a88d5
Lucas Cantoni <lukcnt@outlook.com>
2022-08-22 09:56:38 -0300

    Call the function and show a message if the number is prime or not

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ def prime_number_checker(number_to_check):
     for number in range(2, number_to_check):
         if number_to_check % number == 0:
             prime_number = False
+    return prime_number
 
 number_to_check = int(input("Type the number you want to check if tis is a prime number: "))
+if prime_number_checker(number_to_check) == True:
+    print("It's a prime number.")
+else:
+    print("It's not a prime number.")
