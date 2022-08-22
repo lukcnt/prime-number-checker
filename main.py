2f0c2ecb73b0a269547158d63e324f89b25a18ee
Lucas Cantoni <lukcnt@outlook.com>
2022-08-22 09:45:47 -0300

    Make the function return true if it's a prime number or false if it isn't

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 def prime_number_checker(number_to_check):
-    
+    prime_number = True
+    for number in range(2, number_to_check):
+        if number_to_check % number == 0:
+            prime_number = False
 
 number_to_check = int(input("Type the number you want to check if tis is a prime number: "))
